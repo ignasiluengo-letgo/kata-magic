096c77716e0c189ca5fd838235b007236f5f186d
cookinclude <cyril.clochard@gmail.com>
2020-01-17 17:06:20 +0100

    Move tests to phpunit

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -19,4 +19,38 @@ final class GildedRoseTest extends TestCase
         $this->assertEquals(9, $item->quality);
         $this->assertEquals(4, $item->sellIn);
     }
+
+    /** @test */
+    public function updates_normal_items_on_the_sell_date()
+    {
+        $item = GildedRose::of('normal', 10, 0);
+
+        $item->tick();
+
+        $this->assertEquals(8, $item->quality);
+        $this->assertEquals(-1, $item->sellIn);
+    }
+
+    /** @test */
+    public function updates_normal_items_with_a_quality_of_0()
+    {
+        $item = GildedRose::of('normal', 0, 5);
+
+        $item->tick();
+
+        $this->assertEquals(0, $item->quality);
+        $this->assertEquals(4, $item->sellIn);
+    }
+
+    /** @test */
+    public function updates_brie_items_before_the_sell_date()
+    {
+        $item = GildedRose::of('Aged Brie', 10, 5);
+
+        $item->tick();
+
+        $this->assertEquals(11, $item->quality);
+        $this->assertEquals(4, $item->sellIn);
+    }
+
 }
